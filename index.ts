746a083628a4a529d579a0b3df82a1ae7c5d0db3
johnmarco <johnmarcotasillo@gmail.com>
2023-05-26 14:06:42 -0400

    WOP

--- index.ts
+++ index.ts
@@ -3,5 +3,7 @@ import fs from "fs"
 fs.readFileSync("lines").
     toString().
     split("\n").
+    filter((_, i) => i % 2 === 0).
+    filter((_, i) => i > 1 && i < 4).
     forEach(line => console.log(line));
 
--- main.rs
+++ main.rs
@@ -4,5 +4,9 @@ fn main() {
 
     file
         .lines()
-        .for_each(|line| println!("{}", line));
+        .enumerate()
+        .filter(|(idx, _)| idx % 2 == 0)
+        .skip(2)
+        .take(2)
+        .for_each(|(_, line)| println!("{}", line));
 }
