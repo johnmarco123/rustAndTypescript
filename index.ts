86e41a17a3f47fd748290595857acfa43b647323
johnmarco <johnmarcotasillo@gmail.com>
2023-05-26 11:12:22 -0400

    WOP

---
+++ index.ts
@@ -0,0 +1,7 @@
+import fs from "fs"
+
+fs.readFileSync("lines").
+    toString().
+    split("\n").
+    forEach(line => console.log(line));
+
---
+++ main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let file = std::fs::read_to_string("lines").unwrap();
+
+    file
+        .lines()
+        .for_each(|line| println!("{}", line));
+}
